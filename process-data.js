7a0f6fbea31ec07f1137943d82ae4467892bcd39
Daniel Shiffman <daniel.shiffman@gmail.com>
2025-03-18 20:12:25 -0400

    include image metadata and embeddings

--- process-data.js
+++ process-data.js
@@ -49,46 +49,63 @@ const vision_model = await CLIPVisionModelWithProjection.from_pretrained(
   'Xenova/clip-vit-base-patch16',
   {
     // device: 'webgpu',
-    dtype: 'q8',
+    dtype: 'fp32',
     progress_callback: (x) => process.stdout.write(x.status + ' ' + x.progress + '\r'),
   }
 );
 
-const photos = await readTSV('unsplash/photos.tsv000');
-console.log(`Loaded ${photos.length} photos`);
+const photosTSV = await readTSV('unsplash/photos.tsv000');
+console.log(`Processing ${photosTSV.length} photos from unsplash`);
 
-let dbase = {};
+let photos = [];
+let embeddings = [];
 
-// check if dbase file exists already
-if (fs.existsSync('public/embeddings/dbase.json')) {
-  console.log('Loading existing dbase.json');
-  dbase = JSON.parse(fs.readFileSync('public/embeddings/dbase.json'));
-  console.log(`Loaded ${Object.keys(dbase).length} embeddings`);
-}
+for (let i = 0; i < photosTSV.length; i++) {
+  const photo = photosTSV[i];
 
-// for (let i = 0; i < photos.length; i++) {
-for (let i = 0; i < 500; i++) {
-  const photo = photos[i];
-  // add URL parameter ?w=512&q=80 to resize and compress the image
+  // Adding URL parameter ?w=512&q=80 to resize and compress the image
   const imageUrl = `${photo.photo_image_url}?w=1024&h=1024&fit=max&q=90`;
   const filename = `${photo.photo_id}.jpg`;
-
   const filePath = path.resolve(__dirname, 'public/images', filename);
-  if (dbase[photo.photo_id] && fs.existsSync(filePath)) {
-    process.stdout.write(`Skipping ${filename}\r`);
-    continue;
+
+  // Only download the image if it doesn't exist
+  if (!fs.existsSync(filePath)) {
+    log(`Downloading ${filename}: ${i + 1}/${photos.length}\r`);
+    await downloadImage(imageUrl, filename);
   }
 
-  process.stdout.write(`Downloading ${filename}: ${i + 1}/${photos.length}\r`);
-  await downloadImage(imageUrl, filename);
-
+  // Process the embedding
   const image = await RawImage.read(imageUrl);
   const image_inputs = await processor(image);
-
   const { image_embeds } = await vision_model(image_inputs);
   const image_embeddings = image_embeds.normalize().tolist();
-  dbase[photo.photo_id] = image_embeddings[0];
+
+  // Add to arrays
+  embeddings.push(image_embeddings[0]);
+  photos.push({ id: photo.photo_id, url: photo.photo_image_url });
+
+  log(`Computed embeddings: ${i + 1}/${photosTSV.length}\r`);
+
+  // Save every 500 cycles since it takes a long time to run
+  if (i % 500 == 0) {
+    writeData();
+  }
 }
-console.log('');
-fs.writeFileSync('public/embeddings/dbase.json', JSON.stringify(dbase));
-console.log(`Saved embeddings to dbase.json`);
+
+// If we get to the end
+writeData();
+
+function writeData() {
+  let flattened = embeddings.flat();
+  const embeddingsBuffer = Buffer.from(new Float32Array(flattened).buffer);
+  fs.writeFileSync('public/embeddings/embeddings.bin', embeddingsBuffer);
+  fs.writeFileSync('public/embeddings/photo.json', JSON.stringify(photos));
+  console.log(`Saved ${embeddings.length} embeddings and photo IDs.`);
+}
+
+// Helper function to log progress
+function log(message) {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(message);
+}
--- public/sketch.js
+++ public/sketch.js
@@ -11,16 +11,18 @@ async function setup() {
   createCanvas(100, 20);
   background(127);
   let textInput = createInput('cat');
+
+  // Loading Transformers.js
   let tfjs = await loadTransformers();
   let { AutoTokenizer, CLIPTextModelWithProjection, cos_sim } = tfjs;
 
-  console.log('load text model');
+  // Loading the model
   const tokenizer = await AutoTokenizer.from_pretrained('Xenova/clip-vit-base-patch16');
   const text_model = await CLIPTextModelWithProjection.from_pretrained(
     'Xenova/clip-vit-base-patch16',
     {
       device: 'webgpu',
-      dtype: 'q8',
+      dtype: 'fp16',
       progress_callback: (x) => {
         fill(0);
         let w = x.process * 100 || 100;
@@ -29,40 +31,56 @@ async function setup() {
     }
   );
 
+  // Loading the embeddings
+  let rawData = await loadBytes('embeddings/embeddings.bin');
+  let rawFloats = new Float32Array(rawData.buffer, rawData.byteOffset, rawData.byteLength / 4);
+  const length = 512;
+  const total = rawFloats.length / length;
+  let embeddings = [];
+  for (let i = 0; i < total; i++) {
+    let start = i * length;
+    let embedding = rawFloats.slice(start, start + length);
+    embeddings.push(embedding);
+  }
+
+  // Loading photo metadata
+  let photos = await loadJSON('embeddings/photo.json');
+  console.log(`loaded embeddings for ${photos.length} images`);
+
   let submit = createButton('submit');
   let imagesDiv = createDiv();
   submit.mousePressed(async () => {
     // clear imagesDiv
     imagesDiv.html('');
 
-    const texts = [textInput.value()];
+    const texts = [textInput.value().trim()];
     const text_inputs = tokenizer(texts, { padding: true, truncation: true });
+
+    // console.log('Input text:', texts);
+    // console.log('Token IDs:', text_inputs.input_ids.tolist());
 
     // Compute embeddings
     const { text_embeds } = await text_model(text_inputs);
     const text_embeddings = text_embeds.normalize().tolist();
-    console.log(text_embeddings);
-
-    let dbase = await loadJSON('embeddings/dbase.json');
-    let keys = Object.keys(dbase);
 
     let similarities = [];
-    for (let i = 0; i < keys.length; i++) {
-      const imageKey = keys[i];
-      const image_embeddings = dbase[imageKey];
+    for (let i = 0; i < embeddings.length; i++) {
+      const image_embeddings = embeddings[i];
+      const { id, url } = photos[i];
       const similarity = cos_sim(text_embeddings[0], image_embeddings);
-      similarities.push({ imageKey, similarity });
+      similarities.push({ id, url, similarity });
     }
 
     similarities.sort((a, b) => b.similarity - a.similarity);
     // console.log(similarities);
 
-    for (let i = 0; i < 10; i++) {
-      const { imageKey, similarity } = similarities[i];
-      const img = createImg(`images/${imageKey}.jpg`, 'image from unsplash');
+    for (let i = 0; i < 20; i++) {
+      const { id, url, similarity } = similarities[i];
+      // const img = createImg(`images/${photo_id}.jpg`, 'image from unsplash');
+      // const img = createImg(`images/${id}.jpg`, 'image from unsplash');
+      const img = createImg(url + '?w=512&h=512&fit=max&q=90', 'image from unsplash');
       img.parent(imagesDiv);
       img.size(128, 128);
-      console.log(imageKey, similarity);
     }
   });
 }
